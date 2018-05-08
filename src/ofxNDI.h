18dd5fc3fce0093fdc95aad5f3f2912e3fe49eee
ISHII 2bit <i@2bit.jp>
2018-05-08 19:05:26 +0900

    add std

--- src/ofxNDI.h
+++ src/ofxNDI.h
@@ -30,7 +30,7 @@ namespace ofxNDI {
 			return {};
 		}
 		
-		auto getSourceInfo = [](uint32_t waittime_ms, bool local, const string &group, const vector<string> extra_ips) -> vector<Source> {
+        auto getSourceInfo = [](uint32_t waittime_ms, bool local, const std::string &group, const std::vector<std::string> &extra_ips) -> std::vector<Source> {
 			const NDIlib_find_create_t settings = {local, group.c_str(), ofJoinString(extra_ips, ",").c_str()};
 			NDIlib_find_instance_t finder = NDIlib_find_create_v2(&settings);
 			if (!finder) return {};
@@ -43,10 +43,10 @@ namespace ofxNDI {
 			}
 			
 			sources = NDIlib_find_get_current_sources(finder, &num_sources);
-			vector<Source> ret;
-			ret.insert(end(ret), sources, sources+num_sources);
+			std::vector<Source> ret;
+			ret.insert(std::end(ret), sources, sources+num_sources);
 			for(auto &r : ret) {
-				cout << "NDI Source Detected : " << r.p_ndi_name << "," << r.p_url_address << endl;
+                std::cout << "NDI Source Detected : " << r.p_ndi_name << "," << r.p_url_address << std::endl;
 			}
 			NDIlib_find_destroy(finder);
 			return ret;
@@ -60,11 +60,11 @@ namespace ofxNDI {
 			case Location::LOCAL: {
 				auto&& both = getSourceInfo(waittime_ms, true, group, extra_ips);
 				auto&& remote = getSourceInfo(waittime_ms, false, group, extra_ips);
-				both.erase(remove_if(begin(both),end(both),[&remote](const Source &s){
-					return find_if(begin(remote), end(remote), [&s](const Source &r){
+				both.erase(std::remove_if(std::begin(both), std::end(both),[&remote](const Source &s){
+					return std::find_if(std::begin(remote), std::end(remote), [&s](const Source &r){
 						return s.p_ndi_name == r.p_ndi_name && s.p_url_address == r.p_url_address;
-					}) != end(remote);
-				}), end(both));
+					}) != std::end(remote);
+				}), std::end(both));
 				return both;
 			}
 		}
