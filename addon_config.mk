2baa673a9585e533ca05a8e09319c6d6d12c1152
ISHII 2bit <ishii@buffer-renaiss.com>
2018-05-23 17:01:58 +0900

    fix mistake

--- addon_config.mk
+++ addon_config.mk
@@ -7,13 +7,14 @@ meta:
 
 common:
 	ADDON_INCLUDES = libs/NDI/include
+	ADDON_INCLUDES += libs/utils
 osx:
 	ADDON_LDFLAGS = -Xlinker -rpath -Xlinker @executable_path
 vs:
 	# x64
-	ADDON_LIBS = libs/NDI/lib/vs/x64/Processing.NDI.Lib.x64.lib
-	ADDON_DLLS_TO_COPY = windows-copy-to-bin/Processing.NDI.Lib.x64.lib
+	ADDON_LIBS += libs/NDI/lib/vs/x64/Processing.NDI.Lib.x64.lib
+	ADDON_DLLS_TO_COPY += windows-copy-to-bin/Processing.NDI.Lib.x64.dll
 	# Win32
-	# ADDON_LIBS = libs/NDI/lib/vs/Win32/Processing.NDI.Lib.x86.lib
-	# ADDON_DLLS_TO_COPY = windows-copy-to-bin/Processing.NDI.Lib.x86.lib
+	ADDON_LIBS += libs/NDI/lib/vs/Win32/Processing.NDI.Lib.x86.lib
+	ADDON_DLLS_TO_COPY += windows-copy-to-bin/Processing.NDI.Lib.x86.dll
 
