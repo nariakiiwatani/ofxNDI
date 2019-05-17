df374eff56d11b6cdf72d2327542b0ec447b6a5c
nariakiiwatani <nariakiiwatani@annolab.com>
2019-05-17 09:38:27 +0900

    update receiver examples to select sender source

--- example-receiver-ios/src/ofApp.mm
+++ example-receiver-ios/src/ofApp.mm
@@ -1,10 +1,28 @@
 #include "ofApp.h"
 
+using namespace std;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 	ofBackground(0);
 	ofSetFrameRate(60);
-	if(receiver_.setup()) {
+	auto findSource = [](const string &name_or_url) {
+		auto sources = ofxNDI::listSources();
+		if(name_or_url == "") {
+			return make_pair(ofxNDI::Source(), false);
+		}
+		auto found = find_if(begin(sources), end(sources), [name_or_url](const ofxNDI::Source &s) {
+			return ofIsStringInString(s.p_ndi_name, name_or_url) || ofIsStringInString(s.p_url_address, name_or_url);
+		});
+		if(found == end(sources)) {
+			ofLogWarning("ofxNDI") << "no NDI source found by string:" << name_or_url;
+			return make_pair(ofxNDI::Source(), false);
+		}
+		return make_pair(*found, true);
+	};
+	string name_or_url = "";	// Specify name or address of expected NDI source. In case of blank or not found, receiver will grab default(which is found first) source.
+	auto result = findSource(name_or_url);
+	if(result.second ? receiver_.setup(result.first) : receiver_.setup()) {
 		video_.setup(receiver_);
 	}
 }
