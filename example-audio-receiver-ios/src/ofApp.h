319bfa8462121a4c533d114be072362912392e51
nariakiiwatani <nariakiiwatani@annolab.com>
2019-10-01 16:12:23 +0900

    added iOS audio receiver example

---
+++ example-audio-receiver-ios/src/ofApp.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ofxiOS.h"
+#include "ofxNDIReceiver.h"
+#include "ofxNDIRecvStream.h"
+
+class ofApp : public ofxiOSApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs & touch);
+	void touchMoved(ofTouchEventArgs & touch);
+	void touchUp(ofTouchEventArgs & touch);
+	void touchDoubleTap(ofTouchEventArgs & touch);
+	void touchCancelled(ofTouchEventArgs & touch);
+	
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+	
+	void audioOut(ofSoundBuffer &buffer);
+private:
+	ofxNDIReceiver receiver_;
+	ofxNDIRecvAudioFrameSync audio_;
+	ofSoundStream stream_;
+};
---
+++ example-audio-receiver-ios/src/ofApp.mm
@@ -0,0 +1,108 @@
+#include "ofApp.h"
+
+using namespace std;
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	auto findSource = [](const std::string &name_or_url) {
+		auto sources = ofxNDI::listSources();
+		if(name_or_url == "") {
+			return make_pair(ofxNDI::Source(), false);
+		}
+		auto found = find_if(begin(sources), end(sources), [name_or_url](const ofxNDI::Source &s) {
+			return ofIsStringInString(s.p_ndi_name, name_or_url) || ofIsStringInString(s.p_url_address, name_or_url);
+		});
+		if(found == end(sources)) {
+			ofLogWarning("ofxNDI") << "no NDI source found by string:" << name_or_url;
+			return make_pair(ofxNDI::Source(), false);
+		}
+		return make_pair(*found, true);
+	};
+	std::string name_or_url = "";	// Specify name or address of expected NDI source. In case of blank or not found, receiver will grab default(which is found first) source.
+	auto result = findSource(name_or_url);
+	if(result.second ? receiver_.setup(result.first) : receiver_.setup()) {
+		int sample_rate = 48000;
+		int num_channels = 2;
+		int num_buffers = 1;
+		int num_samples = 512;
+		audio_.setup(receiver_);
+		audio_.setSampleRate(sample_rate);
+		audio_.setNumChannels(num_channels);
+		audio_.setNumSamples(num_samples);
+		ofSoundStreamSettings sss;
+		sss.numInputChannels = 0;
+		sss.numOutputChannels = num_channels;
+		sss.sampleRate = sample_rate;
+		sss.numBuffers = num_buffers;
+		sss.bufferSize = num_samples;
+		sss.setOutListener(this);
+		stream_.setup(sss);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+}
+
+//--------------------------------------------------------------
+void ofApp::audioOut(ofSoundBuffer &buffer){
+	if(receiver_.isConnected()) {
+		audio_.update();
+		if(audio_.isFrameNew()) {
+			audio_.decodeTo(buffer);
+		}
+	}
+}
+//--------------------------------------------------------------
+void ofApp::exit(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDown(ofTouchEventArgs & touch){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::touchMoved(ofTouchEventArgs & touch){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::touchUp(ofTouchEventArgs & touch){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDoubleTap(ofTouchEventArgs & touch){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::touchCancelled(ofTouchEventArgs & touch){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::lostFocus(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::gotFocus(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMemoryWarning(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::deviceOrientationChanged(int newOrientation){
+	
+}
