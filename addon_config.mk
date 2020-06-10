bead9a70d584783b57f3a7b48d80634ac326f5d4
Hiroshi Matoba <matobahiroshi@gmail.com>
2020-06-10 12:24:58 +0200

    remove ADDON_LIBS beause PG can automatically detect

--- addon_config.mk
+++ addon_config.mk
@@ -14,9 +14,7 @@ osx:
 	ADDON_LDFLAGS = -Xlinker -rpath -Xlinker @executable_path
 vs:
 	# x64
-	ADDON_LIBS += libs/NDI/lib/vs/x64/Processing.NDI.Lib.x64.lib
 	ADDON_DLLS_TO_COPY += windows-copy-to-bin/Processing.NDI.Lib.x64.dll
 	# Win32
-	ADDON_LIBS += libs/NDI/lib/vs/Win32/Processing.NDI.Lib.x86.lib
 	ADDON_DLLS_TO_COPY += windows-copy-to-bin/Processing.NDI.Lib.x86.dll
 
