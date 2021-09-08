f1a76f249eb5a68570bd280a99f9e38bfacd715c
nariakiiwatani <nariakiiwatani@annolab.com>
2021-09-08 12:13:39 +0900

    [WIP] update router

--- src/ofxNDIRouter.cpp
+++ src/ofxNDIRouter.cpp
@@ -8,7 +8,7 @@ bool ofxNDIRouter::setup(const std::string &name, const std::string &group)
 		name.c_str(),
 		group.c_str()
 	};
-	instance_ = NDIlib_routing_create(&info);
+	instance_ = NDIlib_routing_create_v2(&info);
 	return instance_ != nullptr;
 }
 bool ofxNDIRouter::setRoute(const Source &source) const
@@ -16,6 +16,21 @@ bool ofxNDIRouter::setRoute(const Source &source) const
 	auto src = toV1(source);
 	return NDIlib_routing_change(instance_, &src);
 }
+
+void ofxNDIRouter::addConnectionMetadata(const std::string &metadata, int64_t timecode) const
+{
+	const NDIlib_metadata_frame_t data = {
+		static_cast<int>(metadata.length()+1),
+		timecode,
+		const_cast<char*>(metadata.c_str())
+	};
+	NDIlib_routing_add_connection_metadata(instance_, &data);
+}
+void ofxNDIRouter::clearConnectionMetadata() const
+{
+	NDIlib_routing_clear_connection_metadata(instance_);
+}
+
 Source ofxNDIRouter::getSource() const
 {
 	return toV2(*NDIlib_routing_get_source_name(instance_));
--- src/ofxNDIRouter.h
+++ src/ofxNDIRouter.h
@@ -13,6 +13,10 @@ public:
 
 	bool setup(const std::string &name, const std::string &group="");
 	bool setRoute(const ofxNDI::Source &source) const;
+
+	void addConnectionMetadata(const std::string &metadata, int64_t timecode=NDIlib_send_timecode_synthesize) const;
+	void clearConnectionMetadata() const;
+
 	ofxNDI::Source getSource() const;
 	int getNumConnections(uint32_t timeout=0) const;
 	bool clear() const;
